9c4e58a640e7ac07f97328dfc575813b30d8035a
thekonst <>
2002-02-13 15:25:26 +0000

    Made it a little bit harder for user to add theirselves to contact list. Caused problems before. Now works great for me allowing to fetch self details without a problem.

--- src/Client.cpp
+++ src/Client.cpp
@@ -1888,6 +1888,7 @@ namespace ICQ2000 {
    */
   void Client::addContact(Contact& c) {
 
+    if (m_self.getUIN() != c.getUIN())
     if (!m_contact_list.exists(c.getUIN())) {
 
       Contact& m_contact = m_contact_list.add(c);
@@ -2008,7 +2009,9 @@ namespace ICQ2000 {
    * that uin exists on your list.
    */
   Contact* Client::getContact(const unsigned int uin) {
-    if (m_contact_list.exists(uin)) {
+    if (uin == m_self.getUIN()) {
+      return &m_self;
+    } else if (m_contact_list.exists(uin)) {
       return &m_contact_list[uin];
     } else {
       return NULL;
